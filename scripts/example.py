9d937a3275fba9bd25195bedb0607eb89442472f
Nico Spiller <nspiller1@mailbox.org>
2023-11-22 10:38:21 -0500

    jupytext example

---
+++ scripts/example.py
@@ -0,0 +1,27 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: video_analysis
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # instructions
+# Convert this script file to jupyter notebook with
+# [jupytext](https://github.com/mwouts/jupytext)
+# (there is also a [vscode extenstion](https://marketplace.visualstudio.com/items?itemName=congyiwu.vscode-jupytext) ).
+
+# %%
+# import from locally installed module
+from src.video import adjust_crop
+
+# %%
