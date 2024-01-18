15d96cbd6b35049390e5c931f18e44924c5ef8ff
Nico Spiller <nspiller1@mailbox.org>
2024-01-18 17:45:19 -0500

    done with left right analysis

--- scripts/side_preference.py
+++ scripts/side_preference.py
@@ -15,7 +15,11 @@
 # ---
 
 # %%
+# %load_ext autoreload
+# %autoreload 2
+
 from pathlib import Path
+import pandas as pd
 
 from src import (
     fly_arena as fa
@@ -23,13 +27,49 @@ from src import (
 
 
 # %%
-ps = [Path('./right_left/starved-bb/starved-bb-track.mat')]
-print(ps)
+p_mat = Path('./right_left/starved-bb/starved-bb-track.mat')
+p_sep = './right_left/coordinates.txt'
+p_frame = './right_left/frame.png'
+
+p_out = p_mat.parent / f'{p_mat.stem}_side_analysis'
+p_out.mkdir(exist_ok=True)
+
 
 # %%
-p = ps[0]
 
-data = fa.load_track(p)
+data = fa.load_track(p_mat)
 
+sep = fa.load_separation(p_sep)
+
+line = fa.get_line(sep)
+
+results = fa.get_sides(data, line)
+
+
+# %%
+
+df = pd.DataFrame({
+    'frames_left' : pd.Series(dtype=int),
+    'frames_right': pd.Series(dtype=int),
+    'ratio' : pd.Series(dtype=float),
+    'dropped_frames' : pd.Series(dtype=int),
+})
+
+
+df.index.name = 'fly'
+
+for i, res in results.items():
+    
+    fly = i + 1
+
+    fa.plot_sides(p_frame, sep, line, res, p_out / f'fly_{fly}.png')
+
+    fl, fr = res['left_mask'].sum(), res['right_mask'].sum()
+    ratio = fr / fl if fl > 0 else 0
+    nan = res['nan_frames']
+
+    df.loc[fly, :] = [ fl, fr, ratio, nan ]
+
+df
 
 # %%
--- src/fly_arena.py
+++ src/fly_arena.py
@@ -1,12 +1,17 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
 from scipy.io import loadmat
 import h5py
+
+from PIL import Image
 
 
 def load_track(matlab_file):
     # https://github.com/jstaf/fly_tracker
 
 
-        # load tracking data
     try:
         # matlab file pre 7.3
         m = loadmat(matlab_file, squeeze_me=True, struct_as_record=False)
@@ -18,3 +23,85 @@ def load_track(matlab_file):
 
     return data
     
+def load_separation(separation_file):
+     
+    sep = np.loadtxt(separation_file).astype(int)
+
+    return sep
+
+def get_line(sep):
+
+    # define line separating left and right
+    line = dict()
+    for p1, p2 in zip(sep, sep[1:]):
+        dx = p2[0] - p1[0]
+        dy = p2[1] - p1[1]
+        pxl = np.max([dx, dy])
+
+        x = np.linspace(p1[0], p2[0], pxl + 1).astype(int)
+        y = np.linspace(p1[1], p2[1], pxl + 1).astype(int)
+
+        line = {**line, **{ j: i for i, j in zip(x, y)}}
+
+    return line
+
+
+
+
+def get_sides(data, line):
+
+    results = dict()
+
+    for fly, xy in enumerate(data):
+
+        # drop nan
+        fnan = np.isnan(xy).any(axis=1)
+        xy = xy[~fnan]
+
+        # get x and y
+        x = xy[:, 0]
+        y = xy[:, 1]
+
+        # masks for left and right of line
+        bl = np.array([line[int(j)] >= i for i, j in zip(x, y)])
+        br = ~bl
+
+        results[fly] = {
+            'x_pxl': x,
+            'y_pxl': y,
+            'left_mask': bl,
+            'right_mask': br,
+            'nan_frames': fnan.sum(),
+        }
+
+    return results
+
+def plot_sides(p_png, sep, line, res, path=''):
+        
+        # load first frame
+        img = Image.open(p_png)
+
+        fig, ax = plt.subplots()
+
+        # plot photo
+        ax.imshow(img)
+
+        # plot line defining points
+        ax.scatter(sep[:,0], sep[:,1], zorder=99, color='k')
+
+        # plot line definition
+        y, x = line.keys(), line.values()
+        ax.plot(x, y, color='C3', zorder=98)
+        
+        # plot trajectories
+        cmap_paired = plt.cm.tab20.colors
+        x, y = res['x_pxl'], res['y_pxl']
+        l, r = res['left_mask'], res['right_mask']
+
+        ax.scatter(x[l], y[l], marker=',', s=1, color=cmap_paired[2*0])
+        ax.scatter(x[r], y[r], marker=',', s=1, color=cmap_paired[2*0 + 1])
+
+        if path:
+            fig.savefig(path)
+            plt.close(fig)
+
